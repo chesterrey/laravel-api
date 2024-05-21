5521cb54acffa848bbf7ad0bd64458f84ee178fd
chesterrey <rey.tan@student.ateneo.edu>
2024-05-21 22:54:10 +0800

    first commit

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function register(Request $request): JsonResponse
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string',
+            'email' => 'required|string|email|unique:users',
+            'password' => 'required|string|confirmed',
+            'password_confirmation' => 'required|string'
+        ]);
+
+        if ($validator->fails()) {
+            return $this->sendError('Validation Error.', $validator->errors());
+        }
+
+        $input = $request->all();
+        $input['password'] = bcrypt($input['password']);
+        $user = User::create($input);
+        $success['token'] = $user->create;
+        $success['name'] = $user->name;
+
+        return $this->sendResponse($success, 'User registered successfully.');
+    }
+
+    public function login(Request $request): JsonResponse
+    {
+        if(Auth::attempt(['email' => $request->email, 'password' => $request->password]))
+        {
+            $user = Auth::user();
+            $success['token'] = $user->createToken('MyApp')->accessToken;
+            $success['name'] = $user->name;
+
+            return $this->sendResponse($success, 'User logged in successfully.');
+        }
+        else
+        {
+            return $this->sendError('Unauthorized.', ['error' => 'Unauthorized']);
+        }
+    }
+}
